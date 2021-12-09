0f86aecbd96c9eff03f8a70fce70d412a6b32c1b
MegablasterMan1 <83662971+MegablasterMan1@users.noreply.github.com>
2021-12-09 11:26:55 -0600

    Calling and seeing planets from swapi

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface SwapiPlanetDataWeCareAbout {
+  next: string;
+  results: {
+    name: string;
+  }[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(
+    private httpSvc: HttpClient
+  ) { }
+
+  loadPlanets = (): Observable<SwapiPlanetDataWeCareAbout> => {
+    const page1 = this.httpSvc.get<SwapiPlanetDataWeCareAbout>('https://swapi.dev/api/planets');
+    return page1;
+  };
+
+
 }
